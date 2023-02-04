651b0317a7a3a55ff199861146da6755e0f96ebc
Khen Solomon Lethil <khensolomon@gmail.com>
2023-02-04 09:28:18 +0100

    Source and Result

---
+++ test/mocha-other.mjs
@@ -0,0 +1,44 @@
+import { it } from "mocha";
+import assert from "assert";
+import fs from "fs";
+import { wordThesaurus } from "../index.mjs";
+
+describe("Source", () => {
+	it("Default source file", () => {
+		assert.strictEqual("./src/thesaurus.dat", wordThesaurus.file);
+		assert.ok(fs.existsSync("./src/thesaurus.dat"));
+	});
+
+	it("Changing source file", () => {
+		wordThesaurus.file = "./tmp.dat";
+		assert.strictEqual("./tmp.dat", wordThesaurus.file);
+		assert.ok(!fs.existsSync("./tmp.dat"));
+	});
+
+	it("Reset source file", () => {
+		wordThesaurus.file = "./src/thesaurus.dat";
+		assert.strictEqual("./src/thesaurus.dat", wordThesaurus.file);
+	});
+});
+
+describe("Result", () => {
+	wordThesaurus.load();
+	it("*.find(love): using toLowerCase", () => {
+		const res = wordThesaurus.find("love");
+		assert.ok(res.length > 0);
+	});
+
+	it("*.find(loVe): testing case insensitive", () => {
+		const res = wordThesaurus.find("loVe");
+		assert.ok(res.length > 0);
+	});
+
+	it("*.find(rapid): result.raw remove duplicate", () => {
+		const res = wordThesaurus.find("rapid");
+		assert.ok(res.length == 2);
+		assert.strictEqual(0, res[0].pos);
+		assert.strictEqual("waterway", res[0].raw.toString());
+		assert.strictEqual(2, res[1].pos);
+		assert.strictEqual("speedy,fast", res[1].raw.toString());
+	});
+});
