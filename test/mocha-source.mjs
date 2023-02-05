e223202ea2753dfe68acf43843bdc619a4a3835e
Khen Solomon Lethil <khensolomon@gmail.com>
2023-02-05 11:49:15 +0100

    update defaultPath

--- test/mocha-source.mjs
+++ test/mocha-source.mjs
@@ -1,23 +1,33 @@
 import { it } from "mocha";
 import assert from "assert";
 import fs from "fs";
+import path from "path";
 
 import thesaurus from "../index.mjs";
 
 describe("Source", () => {
 	it("Default source file", () => {
-		assert.strictEqual("./src/thesaurus.dat", thesaurus.src.file);
+		assert.strictEqual(
+			path.basename("./src/thesaurus.dat"),
+			path.basename(thesaurus.src.file)
+		);
 		assert.ok(fs.existsSync("./src/thesaurus.dat"));
 	});
 
 	it("Changing source file", () => {
 		thesaurus.src.file = "./tmp.dat";
-		assert.strictEqual("./tmp.dat", thesaurus.src.file);
+		assert.strictEqual(
+			path.basename("./tmp.dat"),
+			path.basename(thesaurus.src.file)
+		);
 		assert.ok(!fs.existsSync("./tmp.dat"));
 	});
 
 	it("Reset source file", () => {
 		thesaurus.src.file = "./src/thesaurus.dat";
-		assert.strictEqual("./src/thesaurus.dat", thesaurus.src.file);
+		assert.strictEqual(
+			path.basename("./src/thesaurus.dat"),
+			path.basename(thesaurus.src.file)
+		);
 	});
 });
